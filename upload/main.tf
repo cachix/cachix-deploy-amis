fc5b8a8f98ae7fee98c772bb0fc6a94cb1e6f731
Sander <hey@sandydoo.me>
2023-07-26 14:49:13 +0000

    Hardcode regions

--- upload/main.tf
+++ upload/main.tf
@@ -38,6 +38,25 @@ provider "aws" {
 variable "regions" {
   type = list(string)
   description = "Regions to deploy the AMIs to"
+  default = [
+    "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-northeast-3",
+    "ap-south-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ca-central-1",
+    "eu-central-1",
+    "eu-north-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-west-3",
+    "sa-east-1",
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2"
+  ]
 }
 
 resource "aws_s3_bucket" "cachix-deploy-amis" {
