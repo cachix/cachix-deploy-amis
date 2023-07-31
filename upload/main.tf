c1bcbbe411143a185a1d58c38b66ee829dc84e67
Sander <hey@sandydoo.me>
2023-07-31 23:20:03 +0000

    Debug

--- upload/main.tf
+++ upload/main.tf
@@ -407,7 +407,6 @@ module "copy_ami_us_west_2" {
 output "ami_ids" {
   value = merge(
     { for _, v in aws_ami.cachix_deploy_ami : "${v.tags_all.Release}.eu-central-1.${v.tags_all.System}" => v.id },
-    module.copy_ami_ap_southeast_1.ami,
-    module.copy_ami_ap_southeast_2.ami
+    # module.copy_ami_ap_northeast_1.ami,
   )
 }
